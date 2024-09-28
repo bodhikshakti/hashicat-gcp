6e0a99fa7914f1d5ac6cebbe5a59a8b311b30ce3
Bodhik <bodhik@gmail.com>
2024-09-28 12:55:32 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -3,5 +3,11 @@ module "network" {
   version = "3.4.0"
   network_name = "${var.prefix}-network"
   project_id = var.project
-  subnets = var.subnet_prefix
+  subnets =  [
+  {
+    subnet_name   = "${var.prefix}-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
 }
